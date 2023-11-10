bf38a9f56bf2cb4797345e663307d6e11142a358
Juancho022 <juanma.francesconi15@gmail.com>
2023-11-10 18:35:22 -0300

    changing controller in inventory

--- src/controllers/inventoryController.js
+++ src/controllers/inventoryController.js
@@ -1,8 +1,14 @@
 const productModel = require('../models/product');
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const inventoryController = {
     inventory(req, res) {
-        const products = productModel.getProducts();
-        return res.render('inventory', { products });
+         res.render('inventory', { product });
     }
 }
+
+module.exports = controller;
