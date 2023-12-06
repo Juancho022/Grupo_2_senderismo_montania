923d4b57980c0e849e49ec4d9d275b3f19770c2b
cgon182 <camilagon182@gmail.com>
2023-12-06 19:28:44 -0300

    register routes added

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/userController');
 
 
 router.get('/login', userController.login);
+router.get('/register', userController.register);
 
 //Procesar el login
 router.post('/login', userController.loginProcess);
