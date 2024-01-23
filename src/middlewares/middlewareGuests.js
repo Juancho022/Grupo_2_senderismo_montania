fdefeb13665ab7d804beb0a0b2b73f300b1a20f0
cgon182 <camilagon182@gmail.com>
2024-01-23 11:33:53 -0300

    fix inventory access

--- src/middlewares/middlewareGuests.js
+++ src/middlewares/middlewareGuests.js
@@ -4,8 +4,14 @@ const path = require('path');
 const usersFilePath = path.join(__dirname, '../data/users.json');
 let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
-function forGuests () {
-    
+function forGuests(req, res, next) {
+    const user = req.session.user;
+
+    if (user && user.category === 'guest') {
+        return res.redirect('/user/login'); 
+    }
+
+    next();
 }
 
 module.exports = forGuests;
