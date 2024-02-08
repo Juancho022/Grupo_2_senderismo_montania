ffb4e96506802421d86e367b02c8ac3942f262b0
cgon182 <camilagon182@gmail.com>
2024-02-08 14:19:28 -0300

    product model modified as -price-

--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -61,7 +61,7 @@ module.exports = (sequelize, DataTypes) => {
         });
         Product.hasMany(models.ProductPrice,{
             foreignKey: 'products_id',
-            as: 'prices'
+            as: 'price'
         })
         
     };
