8bdb9f86ca3329480e575937243d60c8e3218d51
matlegli <leglisematias@gmail.com>
2024-02-13 20:42:43 -0300

    adding userControllers, validationMiddleware, new user routes and modifing views login and userList, ading logout to popup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,90 +8,13 @@ const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const { create } = require('domain');
 const cookies = require('cookie-parser');
+const oneMonth = 1000 * 60 * 60 * 24 * 30;
 
 
 const controller = {
-    list: async (req, res) => {
-        try {
-            const users = await db.User.findAll({
-                include: ['roles'],
-                attributes: {
-                    exclude: ['password', 'roles_id']
-                }
-            });
-            res.render('usersList', { users });
-        } catch (error) {
-            res.send(error);
-        }
-    },
-
-    profile: async (req, res) => {
-        try {
-            const user = await db.User.findByPk(req.session.user.id, {
-                attributes: { exclude: ['password'] },
-                include: ['role']
-            });
-            res.render('profile', { user: user.dataValues });
-        } catch (error) {
-
-        }
-    },
-
-
-    edit: async (req, res) => {
-        try {
-            const user = await db.User.findByPk(req.params.id);
-            const roles = await db.Rol.findAll()
-            res.render('userEditForm', { user, roles });
-        } catch (error) {
-            res.send(error);
-        }
-    },
-
-    update: (req, res) => {
-        db.User.update(req.body, { where: { id: req.params.id } })
-            .then(() => {
-                res.redirect('/');
-            })
-            .catch((err) => {
-                res.send(err);
-            });
-    },
-
-    login: (req, res) => {
-        res.render('login');
-    },
-
-    loginProcess: async (req, res) => {
-        try {
-            const user = await db.User.findOne({
-                where: { email: req.body.emailUsuario },
-                include: ['roles'],
-                limit: 1
-            });
-            if (!user) {
-                return res.render('login', { errors: { unauthorize: { msg: 'Usuario y/o contraseña invalidos' } } });
-            }
-            if (!bcrypt.compareSync(req.body.passwordUsuario, user.password)) {
-                return res.render('login', { errors: { unauthorize: { msg: 'Usuario y/o contraseña invalidos' } } });
-            }
-            req.session.user = {
-                email: user.email,
-                id: user.roles.id
-            };
-            if (req.body.recordarUsuario != undefined) {
-                res.cookie('userEmail', req.session.user.email, { maxAge: 30 * 24 * 60 * 60 * 1000 }); // La cookie expirará en 30 días
-            }
-            return res.redirect('/');
-        } catch (error) {
-            res.send(error);
-        }
-    },
-
     register: (req, res) => {
         res.render('register');
     },
-
     registerProcess: async (req, res) => {
         try {
             const userExists = await db.User.findOne({ where: { email: req.body.email } });
@@ -101,7 +24,7 @@ const controller = {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {  //si hay error renderiza la vista del registro con el error de validación
                 return res.render('register', { errors: errors.mapped(), oldData: req.body });
-            } 
+            }
             const newUser = {
                 first_name: req.body.firstName,
                 last_name: req.body.lastName,
@@ -118,18 +41,124 @@ const controller = {
             res.send(error);
         }
     },
-
+    login: async (req, res) => {
+        const userEmailFromCookie = req.cookies.recordarme;
+        if (userEmailFromCookie) {
+            try {
+                const userToLogin = await db.User.findOne({
+                    where: req.body.email
+                });
+                if (userToLogin) {
+                    const { password, ...nonSensibleUserData } = userToLogin;
+                    req.session.user = nonSensibleUserData;
+                    return res.redirect('/user/profile');
+                }
+            } catch (error) {
+                return res.json(error);
+            }
+        }
+        return res.render('login');
+    },
     logout: (req, res) => {
-        // Elimina la propiedad 'user' de la sesión para cerrarla
         req.session.destroy();
-
-        // Borra la cookie de usuario
-        res.clearCookie('userEmail');
-
-        // Redirige al usuario a la página de inicio
+        res.clearCookie("recordarme");
         return res.redirect('/');
     },
+    loginProcess: async (req, res) => {
+        try {
+            const userToLogin = await db.User.findOne({
+                where: { email: req.body.email }
+            });
+            const rememberMe = Boolean(req.body.recordarme);
 
+            if (userToLogin) {
+                const okPassword = bcrypt.compareSync(req.body.password, userToLogin.password);
+
+                if (okPassword) {
+                    const { password, ...nonSensibleUserData } = userToLogin;
+                    req.session.user = nonSensibleUserData;
+                    if (rememberMe) {
+                        res.cookie("recordarme", userToLogin.email, {
+                            maxAge: oneMonth,
+                            secure: true,
+                            httpOnly: true,
+                        });
+                    }
+                    //const redirectRoute = getRedirectRouteByRole(userToLogin.roles_id);
+                    //return res.redirect(redirectRoute);
+                }
+            }
+            return res.render('login', {
+                errors: {
+                    email: {
+                        msg: "El email y/o la contraseña son incorrectos"
+                    }
+                }
+            });
+        } catch (error) {
+            console.log(error)
+            return res.json(error);
+        }
+    },
+    list: async (req, res) => {
+        try {
+            const users = await db.User.findAll({
+                include: ['roles'],
+                attributes: {
+                    exclude: ['password', 'roles_id']
+                }
+            });
+            res.render('usersList', { users });
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    detail: async (req, res) => {
+        try {
+            const user = await db.User.findByPK(req.params.id);
+            return res.render('userDetail', { user });
+        } catch (error) {
+
+        }
+    },
+    profile: async (req, res) => {
+        try {
+            const user = await db.User.findByPk(req.session.user.id, {
+                attributes: { exclude: ['password'] },
+                include: ['role']
+            });
+            res.render('profile', { user: user.dataValues });
+        } catch (error) {
+        }
+    },
+    edit: async (req, res) => {
+        try {
+            const user = await db.User.findByPk(req.params.id);
+            const roles = await db.Rol.findAll()
+            res.render('userEditForm', { user, roles });
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    update: (req, res) => {
+        db.User.update(req.body, { where: { id: req.params.id } })
+            .then(() => {
+                res.redirect('/');
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    },
+    delete: async (req, res) => {
+        try {
+            await db.User.destroy({
+                where: req.params.id
+            });
+            return res.redirect('/users');
+        } catch (error) {
+            res.json(error)
+        }
+    },
+   
 };
-
 module.exports = controller;
