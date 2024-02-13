2cd5b04e87728e76c240e3b5323e2378b3ad42a3
Andrea Toledo <atoledo618@gmail.com>
2024-02-13 16:14:02 -0300

    :lipstick:optimized the inventory counter event code

--- src/public/js/front.js
+++ src/public/js/front.js
@@ -1,30 +1,34 @@
 //Contador
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtener referencias a los elementos HTML
-    let decreaseButton = document.getElementById('decrease');
-    let increaseButton = document.getElementById('increase');
-    let countElement = document.getElementById('count');
+    
+    let decreaseButtons = document.querySelectorAll('.counter #decrease');
+    let increaseButtons = document.querySelectorAll('.counter #increase');
+    let countElements = document.querySelectorAll('.counter #count');
 
-    // Manejar el evento de decremento
-    decreaseButton.addEventListener('click', function () {
-        // Obtener el valor actual y restar 1
-        let currentCount = parseInt(countElement.textContent);
-        let newCount = currentCount - 1;
-
-        // Verificar que el valor no sea menor que 1
-        if (newCount >= 1) {
-            // Actualizar el elemento con el nuevo valor
-            countElement.textContent = newCount;
-        }
+    decreaseButtons.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            let currentCount = parseInt(countElements[index].textContent);
+            let newCount = currentCount - 1;
+            if (newCount >= 0) {
+                countElements[index].textContent = newCount;
+            }
+        });
     });
 
-    // Manejar el evento de incremento
-    increaseButton.addEventListener('click', function () {
-        // Obtener el valor actual y sumar 1
-        let currentCount = parseInt(countElement.textContent);
-        let newCount = currentCount + 1;
-
-        // Actualizar el elemento con el nuevo valor
-        countElement.textContent = newCount;
+    increaseButtons.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            let currentCount = parseInt(countElements[index].textContent);
+            let newCount = currentCount + 1;
+            countElements[index].textContent = newCount;
+        });
     });
 });
+
+
+
+// Obtener referencias a los elementos HTML
+
+// Manejar el evento de decremento
+
+
+// Manejar el evento de incremento
