ad7ed35a21c8b3a88f72e9ef4a422c2b7b6e151c
roci martinez <roomartinez988@gmail.com>
2024-03-03 20:44:49 -0300

    promotion IMG

--- src/public/js/promotion.js
+++ src/public/js/promotion.js
@@ -1,25 +1,49 @@
-const promotionBar = document.getElementById("promotion-bar");
-const promotions = [
-    "¡Promoción especial por tiempo limitado! 20% de descuento en todos los productos. ¡Aprovéchalo ya!",
-    "¡Compra dos y llévate uno gratis! Solo por hoy.",
-    "Descuento del 30% en productos seleccionados. ¡No te lo pierdas!"
-];
+// Barra de publicidad
+document.addEventListener("DOMContentLoaded", function() {
+    const promotionBar = document.getElementById("promotion-bar");
+    const header = document.querySelector("header");
 
-function changePromotion() {
-    const randomIndex = Math.floor(Math.random() * promotions.length);
-    const randomPromotion = promotions[randomIndex];
-    promotionBar.innerHTML = '<div id="promotion-content">' + randomPromotion + '</div>';
-}
-
-setInterval(changePromotion, 5000);
-
-const header = document.querySelector("header");
-
-window.onscroll = function() {
-    if (window.pageYOffset > 0) {
-        // Si el usuario está en la parte superior de la página, mostrar la promoción
+    window.onscroll = function() {
         promotionBar.style.display = "block";
         header.style.top = "5%";
         header.style.position = "absolute";
+        promotionBar.style.display = "block";
+    };
+
+    const promotions = [
+        "¡Promoción especial por tiempo limitado! 20% de descuento en todos los productos. ¡Aprovéchalo ya!",
+        "¡Compra dos y llévate uno gratis! Solo por hoy.",
+        "Descuento del 30% en productos seleccionados. ¡No te lo pierdas!"
+    ];
+
+    function changePromotion() {
+        const randomIndex = Math.floor(Math.random() * promotions.length);
+        const randomPromotion = promotions[randomIndex];
+        promotionBar.innerHTML = '<div id="promotion-content">' + randomPromotion + '</div>';
     }
-}
+
+    setInterval(changePromotion, 5000);
+});
+
+// Carrusel de publicidad
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carousels = document.querySelectorAll(".carousel-container");
+
+    carousels.forEach(function(carousel) {
+      const images = carousel.querySelectorAll(".carousel-image");
+      const interval = 5000; 
+      let index = 0;
+
+      function showImage() {
+        index++;
+        if (index >= images.length) {
+          index = 0;
+        }
+        const offset = -index * 100;
+        carousel.querySelector(".carousel-images").style.transform = `translateX(${offset}%)`;
+      }
+
+      setInterval(showImage, interval);
+    });
+  });
